3efa3776cf0ace5e41a4e2f2643ba30142d69372
ahshihab580 <ahshihab580@gmail.com>
2026-06-23 21:31:53 +0600

    Bubble Sort.cpp
    Implement Bubble Sort algorithm to sort an array.

---
+++ Bubble Sort.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int arr[5] = {5, 3, 4, 1, 2};
+
+    int n = 5;
+
+    for(int i = 0; i < n-1; i++) {
+
+        for(int j = 0; j < n-i-1; j++) {
+
+            // Compare
+            if(arr[j] > arr[j+1]) {
+
+                // Swap
+                int temp = arr[j];
+
+                arr[j] = arr[j+1];
+
+                arr[j+1] = temp;
+            }
+        }
+    }
+
+    cout << "Sorted Array: ";
+
+    for(int i = 0; i < n; i++) {
+
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
